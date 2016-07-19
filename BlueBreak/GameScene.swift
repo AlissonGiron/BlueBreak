d539a62278fb288b131b49cb237b87c3f376de7b
GBelchior <gabriel.acostab@hotmail.com>
2016-07-18 21:06:09 -0300

    Raquete bugada

--- BlueBreak/GameScene.swift
+++ BlueBreak/GameScene.swift
@@ -9,20 +9,32 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var isFingerOnScreen = false
+    var touchLocation: CGFloat = 0.0
+    var paddle: SKSpriteNode!
+    var paddleVelocity = 0.0
+    
+    var lastUpdateTime = 0.0
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        for child in self.children {
+            if child.name == "paddle" {
+                if let child = child as? SKSpriteNode {
+                    self.paddle = child
+                }
+            }
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
+        let touch = touches.first
+        touchLocation = touch!.locationInNode(self).x
+        isFingerOnScreen = true
         
-        for touch in touches {
+        
+        /*for touch in touches {
             let location = touch.locationInNode(self)
             
             let sprite = SKSpriteNode(imageNamed:"Spaceship")
@@ -36,10 +48,45 @@ class GameScene: SKScene {
             sprite.runAction(SKAction.repeatActionForever(action))
             
             self.addChild(sprite)
-        }
+        }*/
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        isFingerOnScreen = false
+        touchLocation = 0.0
+        paddleVelocity = 0.0
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        if (isFingerOnScreen) {
+            paddleVelocity += 2 * (currentTime - lastUpdateTime)
+            MovePaddle(touchLocation)
+        }
+        lastUpdateTime = currentTime
+    }
+    
+    func MovePaddle(touchLocation: CGFloat) {
+        print(paddle.position.x, self.size.width - paddle.size.width / 2)
+        
+        if paddle.position.x <= self.size.width - paddle.size.width / 2 && paddle.position.x - paddle.size.width / 2 >= 0 {
+            
+            if touchLocation < self.size.width / 2 {
+                paddle.position.x -= CGFloat(paddleVelocity)
+            }
+            else {
+                paddle.position.x += CGFloat(paddleVelocity)
+            }
+        }
+        else {
+            //Checar se a raquete passou da borda da tela
+            if paddle.position.x > self.size.width - paddle.size.width / 2 {
+                paddle.position.x = self.size.width - paddle.size.width / 2
+            }
+            else if paddle.position.x - paddle.size.width / 2 < 0 {
+                paddle.position.x = paddle.size.width / 2 + 1
+            }
+        }
     }
 }
