acf39215cde597d3da3b1d28da9422e1d69ecfe2
GBelchior <gabriel.acostab@hotmail.com>
2016-07-18 21:26:13 -0300

    tirar comentarios

--- BlueBreak/GameScene.swift
+++ BlueBreak/GameScene.swift
@@ -19,31 +19,91 @@ class GameScene: SKScene {
     
     override func didMoveToView(view: SKView) {
         super.didMoveToView(view)
+        
+        for child in self.children {
+            if child.name == "paddle" {
+                if let child = child as? SKSpriteNode {
+                    self.paddle = child
+                }
+            }
+        }
+        
+        // 1
+        let cols = 7
+        
+        let numberOfBlocks = 5
+        let blockWidth = SKSpriteNode(imageNamed: "block").size.width
+        let totalBlocksWidth = blockWidth * CGFloat(numberOfBlocks)
+        // 2
+        let xOffset = (CGRectGetWidth(frame) - totalBlocksWidth) / 6
+        // 3
+        
+        for j in 1...cols {
+            for i in 0..<numberOfBlocks {
+                let block = SKSpriteNode(imageNamed: "block.png")
+            
+
+                block.position = CGPoint(x: xOffset + CGFloat(CGFloat(i) * 1.3 + 0.5) * blockWidth,
+                                     y: CGRectGetHeight(frame) * 0.05 * CGFloat(j))
+            
+                block.physicsBody = SKPhysicsBody(rectangleOfSize: block.frame.size)
+                
+            
+                // Deixa os blocos curvados
+                switch i {
+                    case 0:
+                        block.zRotation = 3.4
+                    case 1:
+                        block.position.y += 12
+                        block.zRotation = -3
+                    case 2:
+                        block.position.y += 16
+                    case 3:
+                        block.position.y += 12
+                        block.zRotation = 3
+                    case 4:
+                        block.zRotation = -3.4
+                    default:
+                        break
+                }
+                
+                block.physicsBody!.allowsRotation = false
+                block.physicsBody!.friction = 0.0
+                block.physicsBody!.affectedByGravity = false
+                block.physicsBody!.dynamic = false
+            
+                //block.name = BlockCategoryName
+                //block.physicsBody!.categoryBitMask = BlockCategory
+                block.zPosition = 5
+                addChild(block)
+            }
+        }
+        
+        //ball.physicsBody!.contactTestBitMask = BottomCategory | BlockCategory
+    }
+    
+    /* QUEBRAR BLOCO, FALTA A PARTE DE COLISAO
+    func didBeginContact(contact: SKPhysicsContact) {
+        if firstBody.categoryBitMask == BallCategory && secondBody.categoryBitMask == BlockCategory {
+            breakBlock(secondBody.node!)
+            //TODO: check if the game has been won
+        }
+    }
+    
+    func breakBlock(node: SKNode) {
+        let particles = SKEmitterNode(fileNamed: "BrokenPlatform")!
+        particles.position = node.position
+        particles.zPosition = 3
+        addChild(particles)
+        particles.runAction(SKAction.sequence([SKAction.waitForDuration(1.0), SKAction.removeFromParent()]))
+        node.removeFromParent()
     }
     */
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
         let touch = touches.first
         touchLocation = touch!.locationInNode(self).x
         isFingerOnScreen = true
-        
-        
-        /*for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }*/
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -53,7 +113,6 @@ class GameScene: SKScene {
     }
    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
         
         if (isFingerOnScreen) {
             paddleVelocity += 2 * (currentTime - lastUpdateTime)
