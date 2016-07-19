a43558efa028b472f9e2b59b3ba00ffe9d2d0618
GBelchior <gabriel.acostab@hotmail.com>
2016-07-18 21:17:31 -0300

    dnskjcks

--- BlueBreak/GameScene.swift
+++ BlueBreak/GameScene.swift
@@ -18,15 +18,7 @@ class GameScene: SKScene {
     var lastUpdateTime = 0.0
     
     override func didMoveToView(view: SKView) {
-        
-        /*for child in self.children {
-            if child.name == "paddle" {
-                if let child = child as? SKSpriteNode {
-                    self.paddle = child
-                }
-            }
-        }*/
-        
+        super.didMoveToView(view)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
